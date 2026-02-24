cc1c158d45171d59d23951e9bc8f8fdfbf4ec965
Not-Pinto <liamkaehler@gmail.com>
2026-02-24 17:34:48 +0800

    more

--- mcp-vs-main/src/Robot.c
+++ mcp-vs-main/src/Robot.c
@@ -9,22 +9,15 @@
 
 int main(void)
 {
-  DDRA= 0xFF;//put PORTA into output mode
-  PORTA= 0;
-  int a = 0;
-
-  while(1)
+  DDRA = 0xFF;//put PORTA into output mode
+  PORTA = 0; 
+  while(1)//main loop
   {
-    while (a < 8) //main loop
-    {
-    _delay_ms(50);     //500 millisecond delay
-    PORTA|= (1<<a);  // note here PA3 is just an alias for the number 3
+    _delay_ms(500);     //500 millisecond delay
+    PORTA |= (1<<PA2);  // note here PA3 is just an alias for the number 3
                         // this line is equivalent to PORTA = PORTA | 0b00001000   which writes a HIGH to pin 3 of PORTA
-    _delay_ms(50);
-    PORTA&= ~(1<<a); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
-    a += 1;
-    }
-  a = 0;
+    _delay_ms(500); 
+    PORTA &= ~(1<<PA2); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
   }
-  return(a);
-}//end main
+  return(1);
+}//end main 

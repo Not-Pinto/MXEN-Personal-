7f26650b1fc9f6d6c558eb1b55752db5a6d07e46
Not-Pinto <liamkaehler@gmail.com>
2026-03-10 18:42:56 +0800

    lab 4

--- mcp-vs-main/src/not for tobys eyes.c
+++ mcp-vs-main/src/not for tobys eyes.c
@@ -168,6 +168,9 @@ ISR(INT4_vect)
 */
 
 
+
+
+
 /*
 #include "Robot.h"
 
@@ -216,6 +219,10 @@ ISR(INT4_vect)
 */
 
 
+
+
+
+
 /*
 #include "Robot.h"
 volatile uint8_t edges = 0;
@@ -260,4 +267,84 @@ ISR(INT4_vect)
 {
     edges += 1; 
 }
+*/
+
+
+
+
+
+
+
+/*
+#include "Robot.h"
+#define debounceDelay 
+volatile uint8_t edges = 0;
+
+int main(void)
+{   
+    milliseconds_init();
+    serial0_init();
+
+    DDRA = 0xFF; 
+    PORTA = 0b00000000;
+    DDRE &= ~(1<<PE4); 
+    PORTE |= (1<<PE4); 
+
+    char send[50];
+    uint32_t secs = 0;
+    uint32_t new = 0;
+    uint32_t previousTime = 0;
+
+    cli();
+    EICRB &= ~(1<<ISC40);
+    EICRB |= (1<<ISC41);
+    EIMSK |= (1<<INT4); 
+    sei();
+
+  while(1)
+  {
+    new = milliseconds_now();
+    if ((new - previousTime) >= 1000)
+    {
+        sprintf(send, "Edges recoded in sec %lu: %u\n", secs, edges);
+        serial0_print_string(send);
+        edges = 0;
+        secs += 1;
+        previousTime = new;
+    }
+  }
+  return(1);
+}
+
+ISR(INT4_vect)
+{
+    uint32_t currentTime = milliseconds_now();
+    static uint32_t previousTime = 0;
+    if (currentTime - previousTime > debounceDelay)
+    {
+        previousTime = currentTime;
+        edges += 1;
+    }
+}
+*/
+
+
+
+// for Lab 4 onwards
+/*
+#include "Robot.h"
+
+int main(void)
+  {
+    DDRA |= (1<<PA0);
+    PORTA &= ~(1<<PA0);
+
+    while(1)
+    {
+      PORTA |= (1<<PA0);
+      ms_delay_(500);
+      PORTA &= ~(1<<PA0); 
+      ms_delay_(500); 
+    }
+  }
 */
